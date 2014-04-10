88d67445be2012875170a90d6b75c16f931e5727
Andreas Brommund <andreas.brommund@gmail.com>
2014-04-10 21:51:21 +0200

    custm fileds

--- dixum_wp_plugin_add_game.php
+++ dixum_wp_plugin_add_game.php
@@ -1,151 +1,29 @@
 <?php
 /**
  * Plugin Name: Add game
- * Version: The Plugin's Version Number, e.g.: 1.0
  * Author: Dixum
  * Author URI: www.dixum.se
+ *Description: You need to make 4 custom fields, Pic id, ios, android, and description. Short code: [add_game id="index"] index = id of the post 
+ *
  */
 
- /*  Copyright Dixum
-*/
-function wp_add_game_page(){
-	
-	require_once( ABSPATH . 'wp-admin/includes/image.php' );
-	require_once( ABSPATH . 'wp-admin/includes/file.php' );
-	require_once( ABSPATH . 'wp-admin/includes/media.php' );
-	
+ 
 
-	global $wpdb;
-	$status = '';
-	$sql = 
-			"CREATE TABLE IF NOT EXISTS `wp_games` (
-				`Post_id` int(11) NOT NULL,
-				`Pic_id` int(11) NOT NULL,
-				`Link_android` varchar(120) NOT NULL,
-				`Link_ios` varchar(120) NOT NULL,
-				`Id` int(11) NOT NULL AUTO_INCREMENT,
-				`Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-				PRIMARY KEY (`Id`)
-			)";
-	
-	$wpdb->query($sql);
-	
-	
-	if(isset($_POST['post_game'])&&!empty($_POST['post_game'])){
-	
-		$game_name = htmlentities(trim($_POST['game_name']));
-		$game_description = nl2br(htmlentities(trim($_POST['game_description'])));
-		$game_link_android = $_POST['game_link_android'];
-		$game_link_ios = $_POST['game_link_ios'];
-		
-		if(strrpos($game_link_android, 'http://') === false){
-			$game_link_android = 'http://'.$game_link_android;
-		}
-		if(strrpos($game_link_ios, 'http://') === false){
-			$game_link_ios = 'http://'.$game_link_ios;
-		}
-		if(isset($game_name)&&!empty($game_name)&&isset($game_description)&&!empty($game_description)&&isset($game_link_android)&&!empty($game_link_android)&&isset($game_link_ios)&&!empty($game_link_ios)){
-			if(strlen($game_name) <= 70 && strlen($game_description) <= 700 && strlen($game_link_android) <= 120 && strlen($game_link_ios) <= 120){
-				$file_types_array=array("image/jpeg","image/png");
-				if(in_array($_FILES['game_picture']['type'],$file_types_array)){
-					$upload_overrides = array( 'test_form' => false );
-					
-					$attachment_id = media_handle_upload( 'game_picture', $_POST['post_id'] );
-					
-				
-					$new_post = array(
-								'post_title'	=> $game_name,
-								'post_name'		=> $game_name,
-								'post_content'  => $game_description,
-								'post_status'   => 'publish',
-								'post_type'		=> 'game'
-								);	
-					
-					$post = wp_insert_post($new_post); 
-				
-					if($attachment_id&&$post){
-						
-						$insert = array(	
-									'Post_id' => $post,
-									'Pic_id' => $attachment_id,
-									'Link_android' => $game_link_android,
-									'Link_ios' => $game_link_ios,
-									);
-									
-									
-						if($wpdb->insert('wp_games',$insert)){
-							$status = 'Send';
-						}else{
-							$status = 'Something went wrong';
-						}
-					}else{
-						$status = 'Something went wrong';
-					}
-				}else{
-					$status = 'Invalid file type';
-				}
-			}else{
-				$status = 'Error in length';
-			}
-		}else{
-			$status = 'You must fill in everything!';
-		}
-	}
-
-	ob_start();?>
-	<div class="wrap">
-		<form action="admin.php?page=addgame" method="POST" enctype="multipart/form-data">
-			<h1>Add game</h1>
-			<h2><?php echo ob_get_clean(); echo $status; ob_start();?></h2>
-			<input required type="text" placeholder="Game name" name="game_name" maxlength="50"/><br/>
-			<input required type="file" name="game_picture" id="game_picture"/><br/>
-			<input type="hidden" name="post_id" id="post_id" value="55" />
-			<input required type="text" placeholder="Android link" name="game_link_android" maxlength="100"/><br/>
-			<input required type="text" placeholder="IOS link" name="game_link_ios" maxlength="100"/><br/>
-			<textarea required name="game_description" placeholder="Description" maxlength="400"></textarea><br/>
-			<input type="submit" name="post_game" value="Post game"/> 
-		</form>
-	</div>
-	
-	
-	<?php
-	
-	echo ob_get_clean();
-}
-
-function wp_add_game_tab(){
-	add_submenu_page('edit.php?post_type=game','wp_add_game','Add Game','manage_options','addgame','wp_add_game_page');
-	remove_submenu_page('edit.php?post_type=game', 'post-new.php?post_type=game');
-	
-}
-function get_category_id($cat_name){
-	$term = get_term_by('name', $cat_name, 'category');
-	return $term->term_id;
-}
 function shortcode_add_game($atts){
-	
-	
-	
+
 	$id = $atts['id'];
 	
-	global $wpdb;
+	$post_arr = get_post($post_id,ARRAY_A);
+	$post_metdata = get_metadata('post',$id);
 	
-	$sql = "SELECT * FROM `wp_games` WHERE `Post_id` = '$id'";
-	
-	$thepost = $wpdb->get_row($sql, ARRAY_A);
-	
-	$id =  $thepost['Id'];
-	$post_id = $thepost['Post_id'];
-	$pic_id = $thepost['Pic_id'];
-	$link_android = $thepost['Link_android'];
-	$link_ios = $thepost['Link_ios'];
-	
+	$game_name = $post_arr['post_title'];
+	$description = nl2br($post_metdata['description'][0]);
+	$link_android = $post_metdata['android'][0];
+	$link_ios = $post_metdata['ios'][0];
+	$pic_id= $post_metdata['Pic id'][0];
 	
 	$attachment_url = wp_get_attachment_url($pic_id);
 	$dir = substr($attachment_url,strpos($attachment_url,'/wp-content'),strlen($attachment_url));
-	$post_arr = get_post($post_id,ARRAY_A);
-	$game_name = $post_arr['post_title'];
-	$description = nl2br($post_arr['post_content']);
 	
 	ob_start();
 	$output_string="
@@ -206,15 +84,12 @@ function game_init() {
 		'has_archive'        => true,
 		'hierarchical'       => false,
 		'menu_position'      => null,
-		'supports'           => array( 'title', 'editor', 'author','thumbnail')
+		'supports'           => array( 'title','custom-fields')
 	);
 
-	register_post_type( 'game', $args );
 	
-
-	
+	register_post_type( 'game', $args );	
 }
 add_action( 'init', 'game_init' );
-add_action('admin_menu','wp_add_game_tab');
 add_shortcode('add_game', 'shortcode_add_game' );
 ?>
